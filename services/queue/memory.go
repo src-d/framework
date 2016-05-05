9bbd2c275d9ca110379ce461769a58336d239c40
Miguel Molina <hi@mvader.me>
2016-05-05 10:41:11 +0200

    Add AMPQ to container and memory queue

---
+++ services/queue/memory.go
@@ -0,0 +1,55 @@
+package queue
+
+import "sync"
+
+type memoryBroker struct {
+}
+
+func NewMemoryBroker() Broker {
+	return &memoryBroker{}
+}
+
+func (b *memoryBroker) Queue(name string) (Queue, error) {
+	return &memoryQueue{}, nil
+}
+
+func (b *memoryBroker) Close() error {
+	return nil
+}
+
+type memoryQueue struct {
+	jobs []*Job
+	sync.RWMutex
+}
+
+func (q *memoryQueue) Publish(job *Job) error {
+	q.Lock()
+	defer q.Unlock()
+	q.jobs = append(q.jobs, job)
+	return nil
+}
+
+func (q *memoryQueue) Consume() (JobIter, error) {
+	return &memoryJobIter{q.jobs, 0, &q.RWMutex}, nil
+}
+
+type memoryJobIter struct {
+	jobs []*Job
+	idx  int
+	*sync.RWMutex
+}
+
+func (i *memoryJobIter) Next() (*Job, error) {
+	i.Lock()
+	defer i.Unlock()
+	if len(i.jobs) <= i.idx {
+		return nil, nil
+	}
+	j := i.jobs[i.idx]
+	i.idx++
+	return j, nil
+}
+
+func (i *memoryJobIter) Close() error {
+	return nil
+}
---
+++ services/queue/memory_test.go
@@ -0,0 +1,50 @@
+package queue
+
+import (
+	. "gopkg.in/check.v1"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var _ = Suite(&MemorySuite{})
+
+type MemorySuite struct{}
+
+func (s *MemorySuite) TestIntegration(c *C) {
+	b := NewMemoryBroker()
+
+	q, err := b.Queue(bson.NewObjectId().Hex())
+	c.Assert(err, IsNil)
+
+	job := NewJob()
+	job.Encode(true)
+	err = q.Publish(job)
+	c.Assert(err, IsNil)
+
+	for i := 0; i < 100; i++ {
+		job := NewJob()
+		job.Encode(true)
+		err = q.Publish(job)
+		c.Assert(err, IsNil)
+	}
+
+	i, err := q.Consume()
+	c.Assert(err, IsNil)
+
+	retrievedJob, err := i.Next()
+	c.Assert(err, IsNil)
+
+	var payload bool
+	err = retrievedJob.Decode(&payload)
+	c.Assert(err, IsNil)
+	c.Assert(payload, Equals, true)
+
+	c.Assert(retrievedJob.Id, Equals, job.Id)
+	c.Assert(retrievedJob.Priority, Equals, job.Priority)
+	c.Assert(retrievedJob.Timestamp.Second(), Equals, job.Timestamp.Second())
+
+	err = i.Close()
+	c.Assert(err, IsNil)
+
+	err = b.Close()
+	c.Assert(err, IsNil)
+}
